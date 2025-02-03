a10f5cc61e1ecd8b7319be556c80c30145b2b6cc
nana2503 <87693106+nana2503@users.noreply.github.com>
2025-02-03 15:29:34 +0700

    fix: gg font

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>invoice</div>
+  )
+}
+
+export default page
---
+++ app/ui/font.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana, Noto_Sans_JP } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const noto_sans_jp = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
